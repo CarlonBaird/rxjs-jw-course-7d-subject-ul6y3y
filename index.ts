b7638660df7d8bf3f6ae6daa56ac351484eeb46e
CarlonBaird <carlyestrella@live.com>
2022-11-25 14:27:11 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import { fromEvent, Subject } from 'rxjs';
+
 const emitButton = document.querySelector('button#emit');
 const inputElement: HTMLInputElement = document.querySelector('#value-input');
 const subscribeButton = document.querySelector('button#subscribe');
 
+const value$ = new Subject<string>();
+fromEvent(emitButton, 'click').subscribe(() => value$.next(inputElement.value));
