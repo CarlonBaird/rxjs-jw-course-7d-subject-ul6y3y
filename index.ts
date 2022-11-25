343b731fa26dbf498a1868594d1f499563e97863
CarlonBaird <carlyestrella@live.com>
2022-11-25 14:55:19 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import { fromEvent, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const emitButton = document.querySelector('button#emit');
 const inputElement: HTMLInputElement = document.querySelector('#value-input');
 const subscribeButton = document.querySelector('button#subscribe');
 
 const value$ = new Subject<string>();
-fromEvent(emitButton, 'click').subscribe(() => value$.next(inputElement.value));
+//fromEvent(emitButton, 'click').subscribe(() => value$.next(inputElement.value));
+
+//A subject can be an observable and an observer. It can be passed to the subscribe method
+fromEvent(emitButton, 'click')
+  .pipe(map(() => inputElement.value))
+  .subscribe(value$);
+
+fromEvent(subscribeButton, 'click').subscribe(() => {
+  console.log('New Subscription');
+  value$.subscribe((value) => console.log(value));
+});
